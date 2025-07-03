a8a3625cb3c36b1e8e1c77458961eb948ef3c3d5
Ian King <ian.antking@gmail.com>
2025-07-03 12:04:36 +0100

    check for error when returning README

--- bearprint-api/main.go
+++ bearprint-api/main.go
@@ -26,7 +26,10 @@ func NewApp(printerWriterFactory func() (io.WriteCloser, error)) *App {
 func (a *App) rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write(readmeContent)
+
+	if _, err := w.Write(readmeContent); err != nil {
+		fmt.Println("error writing README:", err)
+	}
 }
 
 func (a *App) healthHandler(w http.ResponseWriter, r *http.Request) {
